2e2ad67e2c0db1bc06e19b6f6d3464cbfbc48ccb
Blake Vente <venteralph@gmail.com>
2021-01-02 01:29:32 -0500

    traverses dirs and extracts files

--- crawler/src/crawler/crawl_pdf.py
+++ crawler/src/crawler/crawl_pdf.py
@@ -6,8 +6,9 @@ Open PDF file contents into directories
 import logging
 
 from pathlib import Path
-from typing import List
+from typing import BinaryIO, Dict, Generator, List, Text
 import pdfplumber
+import pandas as pd
 
 
 __author__ = "Blake Vente"
@@ -24,16 +25,21 @@ class PdfCrawler:
             -  text extracted from pdfs
             -  returns the empty list if file doesn't exist
         """
-        extracted_text = []
+        extracted_pages = []
         try:
             self.validate_pdf(pdf_path)
             with pdfplumber.open(pdf_path) as pdf:
-                extracted_text.extend(map(lambda pg: pg.extract_text(), pdf.pages))
+                extracted_pages.extend(
+                    map(lambda pg: (pg.extract_text() or ""), pdf.pages)
+                )
         except ValueError as e:
-            _logger.log(e)
-            _logger.error(f"pdf {pdf_path} not a file, returning {extracted_text}")
+            _logger.info(e)
+            _logger.error(f"pdf {pdf_path} not a file, returning {extracted_pages}")
 
-        return extracted_text
+        return extracted_pages
+
+    def pdf_to_text(self, pdf_path: Path) -> str:
+        return "\n".join(self.pdf_to_pages(pdf_path)) or ""
 
     def validate_pdf(self, pdf_path: Path) -> bool:
         pdf_name = pdf_path.name
@@ -42,6 +48,55 @@ class PdfCrawler:
             raise ValueError(f"Expected file, given {pdf_name} which is not a file")
 
         if not pdf_path.name.endswith(".pdf"):
+            raise ValueError(f"Expected filename ending with pdf, given {pdf_name}")
+
+        return True
+
+
+class DirCrawler(PdfCrawler):
+    def dir_to_index(self, dir_to_pdf_dir: Path) -> pd.DataFrame:
+        """
+        Args:
+            dir_dir_pdf: like DIR in DIR/SUBDIR/EXAMPLE.pdf
+
+        Returns:
+            pd.DataFrame of ['fname', 'syllabus_text']
+        """
+        index = pd.DataFrame(columns=("course_id", "id", "syllabus_text"))
+        for pdf_dir in dir_to_pdf_dir.iterdir():
+            try:
+                syllabi = self.dir_to_pdf_contents(pdf_dir)
+                for course_id in syllabi:
+                    for i, syllabus in enumerate(syllabi[course_id]):
+                        index.loc[len(index)] = [course_id, i, syllabus]
+
+            except (ValueError, TypeError) as e:
+                _logger.info(e)
+                _logger.error(f"Processing failed for {dir_to_pdf_dir}")
+        return index
+
+    def dir_to_pdf_contents(self, valid_pdf_dir: Path) -> Dict[str, str]:
+        """
+        Args:
+            valid_pdf_dir: a checked directory
+
+        Returns:
+            {'dir_name' : List[PDFContents]}
+        """
+        return {
+            valid_pdf_dir.name: [
+                self.pdf_to_text(pdf) for pdf in valid_pdf_dir.iterdir()
+            ]
+        }
+
+    def validate_dir(self, pdf_dir: Path) -> bool:
+
+        pdf_name = pdf_dir.name
+
+        if not pdf_dir.is_dir:
+            raise ValueError(f"Expected dir, given {pdf_name} which is not a dir")
+
+        if not pdf_dir.name.endswith(".pdf"):
             raise ValueError(f"Expected filename ending with pdf, given {pdf_name}")
 
         return True

fadcfe5bcac9a55f7c8172f20756b0fc97e52b50
Bouba <boubacar.diallo1@macaulay.cuny.edu>
2021-01-19 21:10:57 -0500

    Add frontend styling framework and switch database configs (#11)
    * add material ui and swap out firebase config with mongo

---
+++ api/config/database.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { dbUrl } from "./keys";
+
+export default async function connectToDatabase() {
+  try {
+    await mongoose.connect(dbUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+    console.log(`Database connected to ${dbUrl}`);
+  } catch (err) {
+    console.error(err);
+  }
+}

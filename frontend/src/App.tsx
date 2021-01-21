6dd2e0110cb386fdcecb4ad80dd222604bd2b7b7
Blake Vente <venteralph@gmail.com>
2021-01-21 03:54:54 -0500

    restrict col width to approx that of the search bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,7 @@ import Home from "./pages/Home";
 
 const theme = createMuiTheme({
   shape: {
-    borderRadius: 8,
+    borderRadius: 20,
   },
   props: {
     MuiButtonBase: {
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -16,10 +16,17 @@ import {
   IconButton,
   AppBar,
   Fade,
+  Hidden,
 } from "@material-ui/core";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import SearchIcon from "@material-ui/icons/Search";
 import debounce from "lodash/debounce";
+
+interface ListEntry {
+  visible: boolean;
+  course_id: string;
+  course_name: string;
+}
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -32,6 +39,12 @@ const useStyles = makeStyles((theme) => ({
     top: "auto",
     bottom: 0,
   },
+  centralColumn: {
+    width: theme.spacing(50),
+  },
+  spaced: {
+    margin: theme.spacing(3),
+  },
 }));
 
 const fuse = new Fuse(syllabi, {
@@ -39,7 +52,6 @@ const fuse = new Fuse(syllabi, {
   shouldSort: true,
   findAllMatches: true,
   isCaseSensitive: false,
-  // threshold
 });
 
 function Copyright() {
@@ -57,65 +69,73 @@ function Copyright() {
 
 export default function Home() {
   const classes = useStyles();
-  const [results, setResults] = useState(["asdf", "asdf2"]);
+  const [results, setResults] = useState([
+    { visible: true, course_id: "CSCI Bing Bong", course_name: "Bing Studies" },
+  ]);
   const [query, updateQuery] = useState("");
 
   const handleSearch = ({ currentTarget }: any) => {
     updateQuery(currentTarget.value);
-    setResults(results.map(() => ""));
+    setResults(
+      results.map(({ course_id, course_name }: ListEntry) => ({
+        course_id: course_id,
+        visible: false,
+        course_name: course_name,
+      }))
+    );
 
     const res = fuse.search(currentTarget.value);
     setTimeout(() => {
       setResults(
-        res.map(({ item: { course_id, urls } }) =>
-          JSON.stringify({ course_id, urls })
-        )
+        res.map(({ item: { course_id, syllabus } }) => ({
+          course_id: course_id,
+          visible: true,
+          course_name: syllabus.substring(0, 10),
+        }))
       );
-    }, 1000);
+    }, 500);
   };
 
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Typography component="h1" variant="h5">
-          Syllabase
-        </Typography>
-        <Divider variant="middle" />
-        {/* TODO: Animate from narrow width to full width */}
-        <TextField
-          fullWidth
-          autoFocus
-          id="standard-search"
-          label="Search syllabi..."
-          type="search"
-          variant="outlined"
-          onChange={handleSearch}
-          value={query}
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <IconButton>
-                  <SearchIcon />
-                </IconButton>
-              </InputAdornment>
-            ),
-          }}
-        />
-        <List>
-          {results.map((res, i) => (
-            <Fade
-              in={!!res}
-              unmountOnExit={true}
-              style={{ transitionDelay: `${150 * i}ms` }}
-            >
-              <ListItem button divider key={i}>
-                <ListItemText primary={res} />
-              </ListItem>
-            </Fade>
-          ))}
-        </List>
-      </div>
+    <Container component="main" maxWidth="xs" className={classes.paper}>
+      <Typography component="h1" variant="h3" className={classes.paper}>
+        Syllabase
+      </Typography>
+      <Divider variant="fullWidth" color="white" className={classes.spaced} />
+      {/* TODO: Animate from narrow width to full width */}
+      <TextField
+        fullWidth
+        autoFocus
+        id="standard-search"
+        label="Search syllabi..."
+        type="search"
+        variant="outlined"
+        onChange={handleSearch}
+        autoComplete="off"
+        value={query}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <IconButton>
+                <SearchIcon />
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
+      <List>
+        {results.map(({ course_name, visible, course_id }, i) => (
+          <Fade
+            in={!!visible}
+            unmountOnExit={visible}
+            style={{ transitionDelay: `${200 * i}ms` }}
+          >
+            <ListItem button divider key={i} className={classes.centralColumn}>
+              <ListItemText primary={course_name} secondary={course_id} />
+            </ListItem>
+          </Fade>
+        ))}
+      </List>
       <AppBar position="fixed" color="secondary" className={classes.appBar}>
         <Box padding={1}>
           <Copyright />

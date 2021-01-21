5ade5316bdb99f46b5af72ac253031ba224de80a
Blake Vente <venteralph@gmail.com>
2021-01-20 21:21:38 -0500

    frontend skeleton

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,13 +1,60 @@
 import React, { useState, useEffect } from "react";
-import { Container, Typography, Button } from "@material-ui/core";
+import {
+  Container,
+  Typography,
+  Button,
+  Input,
+  Grid,
+  TextField,
+} from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import InputBase from "@material-ui/core/InputBase";
+import Fuse from "fuse.js";
+
+import syllabi from "../../fixtures/syllabi";
+let results: Array<String> = [];
+
+const fuse = new Fuse(syllabi, {
+  keys: ["course_id", "syllabus"],
+  shouldSort: true,
+  findAllMatches: true,
+  isCaseSensitive: false,
+  // threshold
+});
 
 const Home: React.FunctionComponent = () => {
-  const [message, setMessage] = useState("Syllabased");
+  const [results, setResults] = useState(["asdf", "asdf2"]);
+  const [query, updateQuery] = useState("");
+
+  const onSearch = ({ currentTarget }: any) => {
+    updateQuery(currentTarget.value);
+    const res = fuse.search(currentTarget.value);
+    console.log(res);
+    setResults(
+      res.map(({ item: { course_id, urls } }) =>
+        JSON.stringify({ course_id, urls })
+      )
+    );
+    fuse.search(currentTarget.value);
+    console.log(currentTarget);
+  };
 
   return (
     <Container>
-      <Typography variant="h1">Syllabase Home page</Typography>
-      <Typography variant="h1"> {message} </Typography>
+      <Typography variant="h3">Syllabase Home page</Typography>
+      <Container>
+        <TextField
+          id="standard-search"
+          label="Find..."
+          type="search"
+          variant="outlined"
+          onChange={onSearch}
+          value={query}
+        />
+        {results.map((res, i) => (
+          <Container key={i}>{res}</Container>
+        ))}
+      </Container>
     </Container>
   );
 };

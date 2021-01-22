49a667a54bcdd6c25cb6a40eacf55f06cf5e8938
Blake Vente <venteralph@gmail.com>
2021-01-21 20:47:18 -0500

    Feat frontend (#12)
    * add compatability with yarn2
    
    * frontend skeleton
    
    * add global theme
    
    * add components, examples, animations
    
    * scrollbar gives undesired nudge to page contents
    
    * restrict col width to approx that of the search bar
    
    * pkg deps for debounce
    
    * exaggerate border radius on searchbar
    
    * give syllabase _familiar_ colors...?
    
    * found ideal color distribution? team will vote.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,50 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+import { grey, blue } from "@material-ui/core/colors";
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
-import Home from "pages/Home";
+import Home from "./pages/Home";
+
+const theme = createMuiTheme({
+  shape: {
+    borderRadius: 30,
+  },
+  props: {
+    MuiButtonBase: {
+      disableRipple: true, // No more ripple, on the whole application 💣!
+    },
+  },
+  palette: {
+    primary: {
+      main: blue[500],
+    },
+    secondary: {
+      main: grey[100],
+    },
+  },
+  transitions: {
+    duration: {
+      shortest: 450,
+      shorter: 500,
+      short: 550,
+      standard: 600,
+      complex: 675,
+      enteringScreen: 425,
+      leavingScreen: 395,
+    },
+  },
+});
 
 const App: React.FunctionComponent = () => {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Switch>
-          <Route component={Home} />
-        </Switch>
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Switch>
+            <Route component={Home} />
+          </Switch>
+        </BrowserRouter>
+      </ThemeProvider>
     </div>
   );
 };
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,15 +1,157 @@
-import React, { useState, useEffect } from "react";
-import { Container, Typography, Button } from "@material-ui/core";
+import React, { useState } from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Fuse from "fuse.js";
+import syllabi from "../../fixtures/syllabi";
+import {
+  Divider,
+  List,
+  ListItem,
+  Link,
+  ListItemText,
+  IconButton,
+  AppBar,
+  Fade,
+  Hidden,
+} from "@material-ui/core";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import SearchIcon from "@material-ui/icons/Search";
+import debounce from "lodash/debounce";
 
-const Home: React.FunctionComponent = () => {
-  const [message, setMessage] = useState("Syllabased");
+interface ListEntry {
+  visible: boolean;
+  course_id: string;
+  course_name: string;
+}
+const SyllabaseLogo = [
+  { c: "#4787ed", t: "Sy" },
+  { c: "#df523e", t: "ll" },
+  { c: "#f6bd3f", t: "a" },
+  { c: "#4787ed", t: "ba" },
+  { c: "#50a45c", t: "s" },
+  { c: "#df523e", t: "e" },
+];
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(10),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  appBar: {
+    top: "auto",
+    bottom: 0,
+  },
+  centralColumn: {
+    width: theme.spacing(50),
+  },
+  spaced: {
+    margin: theme.spacing(3),
+  },
+}));
+
+const fuse = new Fuse(syllabi, {
+  keys: ["course_id", "syllabus"],
+  shouldSort: true,
+  findAllMatches: true,
+  isCaseSensitive: false,
+});
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      Copyleft &#127279;&nbsp;
+      <Link color="inherit" href="https://hunterosc.org">
+        Open Source Club
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+export default function Home() {
+  const classes = useStyles();
+  const [results, setResults] = useState([
+    { visible: true, course_id: "CSCI Bing Bong", course_name: "Bing Studies" },
+  ]);
+  const [query, updateQuery] = useState("");
+
+  const handleSearch = ({ currentTarget }: any) => {
+    updateQuery(currentTarget.value);
+    setResults(
+      results.map(({ course_id, course_name }: ListEntry) => ({
+        course_id: course_id,
+        visible: false,
+        course_name: course_name,
+      }))
+    );
+
+    const res = fuse.search(currentTarget.value);
+    setTimeout(() => {
+      setResults(
+        res.map(({ item: { course_id, syllabus } }) => ({
+          course_id: course_id,
+          visible: true,
+          course_name: syllabus.substring(0, 10),
+        }))
+      );
+    }, 500);
+  };
 
   return (
-    <Container>
-      <Typography variant="h1">Syllabase Home page</Typography>
-      <Typography variant="h1"> {message} </Typography>
+    <Container component="main" maxWidth="xs" className={classes.paper}>
+      <Typography component="h1" variant="h3" className={classes.paper}>
+        <span>
+          {SyllabaseLogo.map(({ c, t }) => (
+            <span style={{ color: c }}>{t}</span>
+          ))}
+        </span>
+      </Typography>
+      <Divider variant="fullWidth" color="white" className={classes.spaced} />
+      {/* TODO: Animate from narrow width to full width */}
+      <TextField
+        fullWidth
+        autoFocus
+        id="standard-search"
+        // label="Search syllabi..."
+        type="search"
+        variant="outlined"
+        onChange={handleSearch}
+        autoComplete="off"
+        value={query}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <IconButton>
+                <SearchIcon />
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
+      <List>
+        {results.map(({ course_name, visible, course_id }, i) => (
+          <Fade
+            in={!!visible}
+            unmountOnExit={visible}
+            style={{ transitionDelay: `${200 * i}ms` }}
+          >
+            <ListItem button divider key={i} className={classes.centralColumn}>
+              <ListItemText primary={course_name} secondary={course_id} />
+            </ListItem>
+          </Fade>
+        ))}
+      </List>
+      <AppBar position="fixed" color="secondary" className={classes.appBar}>
+        <Box padding={1}>
+          <Copyright />
+        </Box>
+      </AppBar>
     </Container>
   );
-};
-
-export default Home;
+}

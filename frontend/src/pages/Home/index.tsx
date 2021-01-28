455b9264734dd3952e0898a9851627413eaf7f59
Blake Vente <venteralph@gmail.com>
2021-01-28 14:58:43 -0500

    feat: add pdf viewer to frontend (preliminary) (#18)
    * feat: add pdf viewer to frontend (preliminary)
    
    * feat: frontend pdf viewer in stable state

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,32 +1,40 @@
-import React, { useState } from "react";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import Box from "@material-ui/core/Box";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
-import Fuse from "fuse.js";
-import syllabi from "../../fixtures/syllabi";
 import {
+  AppBar,
   Divider,
+  Fade,
+  Grid,
+  IconButton,
+  Link,
   List,
   ListItem,
-  Link,
   ListItemText,
-  IconButton,
-  AppBar,
-  Fade,
-  Hidden,
 } from "@material-ui/core";
+import Box from "@material-ui/core/Box";
+import Container from "@material-ui/core/Container";
 import InputAdornment from "@material-ui/core/InputAdornment";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
 import SearchIcon from "@material-ui/icons/Search";
-import debounce from "lodash/debounce";
+import Fuse from "fuse.js";
+import { uniq } from "lodash";
+import React, { useState } from "react";
+import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
+import syllabi from "../../fixtures/syllabi";
+import Dialog from "@material-ui/core/Dialog";
+import MuiDialogTitle from "@material-ui/core/DialogTitle";
+import CloseIcon from "@material-ui/icons/Close";
 
 interface ListEntry {
   visible: boolean;
   course_id: string;
   course_name: string;
 }
+
+interface ClassAttrs {
+  urls: string[];
+}
+
 const SyllabaseLogo = [
   { c: "#4787ed", t: "Sy" },
   { c: "#df523e", t: "ll" },
@@ -39,8 +47,30 @@ const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(10),
     display: "flex",
-    flexDirection: "column",
+    flex: 1,
     alignItems: "center",
+    flexDirection: "column",
+  },
+  container: {
+    display: "flex",
+  },
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  modalContents: {
+    backgroundColor: theme.palette.background.paper,
+    height: "100vh",
+    width: "100vw",
+    alignItems: "center",
+    flexDirection: "column",
+  },
+  closeButton: {
+    position: "absolute",
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
   },
   appBar: {
     top: "auto",
@@ -61,32 +91,120 @@ const fuse = new Fuse(syllabi, {
   isCaseSensitive: false,
 });
 
-function Copyright() {
+function PDFDocument({ url }: { url: string }) {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }: any) {
+    setNumPages(numPages);
+  }
   return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      Copyleft &#127279;&nbsp;
-      <Link color="inherit" href="https://hunterosc.org">
-        Open Source Club
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
+    <div>
+      <Document
+        file={url}
+        onLoadSuccess={onDocumentLoadSuccess}
+        onLoadError={console.error}
+        renderMode="svg"
+      >
+        <Page
+          pageNumber={pageNumber}
+          renderAnnotationLayer={false}
+          height={800}
+        />
+      </Document>
+      <p>
+        Page {pageNumber} of {numPages}
+      </p>
+      <Box
+        onClick={() =>
+          // wrap pages 1-indexed
+          setPageNumber(((pageNumber || 0) % (numPages || Infinity)) + 1)
+        }
+      >
+        {">"}
+      </Box>
+    </div>
   );
 }
 
-export default function Home() {
+function ModalInnerContents({ crs_id }: { crs_id: string }) {
   const classes = useStyles();
-  const [results, setResults] = useState([
-    { visible: true, course_id: "CSCI Bing Bong", course_name: "Bing Studies" },
-  ]);
+  const txt = syllabi.find(({ course_id }) => course_id === crs_id);
+  return (
+    <Fade in={!!crs_id}>
+      <Grid container className={classes.modalContents} direction="row">
+        <Grid item xs={12} sm={6}>
+          <Typography component="h5" variant="h5" color="textSecondary">
+            {txt?.course_id}
+          </Typography>
+          <List>
+            {uniq(txt?.urls).map((url, i) => (
+              <ListItem
+                button
+                divider
+                key={i}
+                href={url}
+                component="a"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <ListItemText primary={url.replace(/(.{250})..+/, "$1…")} />
+              </ListItem>
+            ))}
+          </List>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <PDFDocument url="http://syllabi.hunterosc.org/assets/courses/CS_127/CS127_ligorio_syllabus_s20.pdf" />
+        </Grid>
+      </Grid>
+    </Fade>
+  );
+}
+
+function BottomBar() {
+  const classes = useStyles();
+  return (
+    <AppBar position="fixed" color="secondary" className={classes.appBar}>
+      <Box padding={1}>
+        <Typography variant="body2" color="textSecondary" align="center">
+          Copyleft &#127279;&nbsp;
+          <Link color="inherit" href="https://hunterosc.org">
+            Open Source Club
+          </Link>{" "}
+          {new Date().getFullYear()}
+          {"."}
+        </Typography>
+      </Box>
+    </AppBar>
+  );
+}
+
+const DefaultSelectedCourse = {
+  visible: true,
+  course_id: "CS_127",
+  course_name: "Intro to Computer Science",
+};
+
+export default function Home(): JSX.Element {
+  const classes = useStyles();
+  const [results, setResults] = useState([DefaultSelectedCourse]);
   const [query, updateQuery] = useState("");
+  const [active_course_id, setOpen] = React.useState("");
+
+  const handleOpen = (course_id: string) => {
+    setOpen(course_id);
+  };
+
+  const handleClose = () => {
+    setOpen("");
+  };
 
   const handleSearch = ({ currentTarget }: any) => {
     updateQuery(currentTarget.value);
     setResults(
       results.map(({ course_id, course_name }: ListEntry) => ({
-        course_id: course_id,
         visible: false,
+        course_id: course_id,
         course_name: course_name,
       }))
     );
@@ -121,8 +239,8 @@ export default function Home() {
         // label="Search syllabi..."
         type="search"
         variant="outlined"
-        onChange={handleSearch}
         autoComplete="off"
+        onChange={handleSearch}
         value={query}
         InputProps={{
           startAdornment: (
@@ -142,16 +260,36 @@ export default function Home() {
             style={{ transitionDelay: `${200 * i}ms` }}
           >
             <ListItem button divider key={i} className={classes.centralColumn}>
-              <ListItemText primary={course_name} secondary={course_id} />
+              <ListItemText
+                primary={course_name}
+                secondary={course_id}
+                onClick={() => handleOpen(course_id)}
+              />
             </ListItem>
           </Fade>
         ))}
       </List>
-      <AppBar position="fixed" color="secondary" className={classes.appBar}>
-        <Box padding={1}>
-          <Copyright />
-        </Box>
-      </AppBar>
+      <Dialog
+        className={classes.modal}
+        open={!!active_course_id}
+        onClose={handleClose}
+        onEscapeKeyDown={handleClose}
+        closeAfterTransition
+        fullScreen
+        fullWidth
+      >
+        <MuiDialogTitle disableTypography>
+          <IconButton
+            aria-label="close"
+            className={classes.closeButton}
+            onClick={handleClose}
+          >
+            <CloseIcon />
+          </IconButton>
+        </MuiDialogTitle>
+        <ModalInnerContents crs_id={active_course_id} />
+      </Dialog>
+      <BottomBar />
     </Container>
   );
 }
